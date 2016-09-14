9a80ba04f1dc9f64337f4a2a734c10e10e075548
alexandre leibler, lisa maccarrigan and nicolas hurtado <alexandre@>
2016-09-14 10:21:09 -0400

    added class and constructor functions with getters and setters

---
+++ src/PalindromeChecker.php
@@ -0,0 +1,36 @@
+<?php
+    class PalindromeChecker
+    {
+        private $check_word;
+        private $check_result;
+
+        function __construct($word)
+        {
+            $this->check_word = $word;
+            $this->check_result = false;
+        }
+
+        function getCheckWord()
+        {
+            return $this->check_word;
+        }
+        function setCheckWord($word_to_check)
+        {
+            $this->check_word = (string) $word_to_check;
+        }
+        function getResult()
+        {
+            return $this->check_result;
+        }
+        function setResult($word_to_check_result)
+        {
+            $this->check_word = (bool) $word_to_check_result;
+        }
+
+        function checkIfPalindrome($word_to_check)
+        {
+            return;
+        }
+    }
+
+?>
