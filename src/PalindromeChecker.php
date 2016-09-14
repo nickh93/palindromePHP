b570fbba34dda0ad08d8c96bb5fc03af06c43611
alexandre leibler, lisa maccarrigan and nicolas hurtado <nicolas@>
2016-09-14 12:02:26 -0400

    add reverseString function

--- src/PalindromeChecker.php
+++ src/PalindromeChecker.php
@@ -27,9 +27,46 @@
             $this->check_word = (bool) $word_to_check_result;
         }
 
+
+        function reverseString($input_string) {
+            $input_length = strlen($input_string);
+
+            //NOTE: $stop_index will be the character before the middle if the word has an odd length.
+
+            //  T A C O C A T
+            //  0 1 2 3 4 5 6
+            //E.G. TACOCAT length is odd so $stop_index should be 2 ("C")
+
+            //  T A C O
+            //  0 1 2 3
+            //E.G. TACO length is even so $stop_index should be 1 ("A")
+
+
+            $stop_index = floor($input_length / 2) - 1;
+
+            for ($char_index = 0; $char_index <= $stop_index; $char_index++) {
+                $temp = $input_string[$char_index];
+
+                //NOTE:
+                //  T A C O
+                //  0 1 2 3
+                //E.G. current char at index 0 then reverse char index is 4 - 0 - 1 = 3 ("O")
+                //    current char at index 1 then reverse char index is 4 - 1 - 1 = 2 ("C")
+                $reverse_char_index = $input_length - $char_index - 1;
+
+                $input_string[$char_index] = $input_string[$reverse_char_index];
+                $input_string[$reverse_char_index] = $temp;
+            }
+
+            return $input_string;
+        }
+
+
         function checkIfPalindrome($word_to_check)
         {
-            if (strlen($word_to_check) == 1) {
+            $reverse_word_to_check = $this->reverseString($word_to_check);
+
+            if ($reverse_word_to_check === $word_to_check) {
                 return true;
             }
             else {
